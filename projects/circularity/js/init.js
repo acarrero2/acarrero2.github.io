daf21233e5a4fa55c63418992940e3b1a7f222e7
acarrero2 <a.carrero@ardencahillacademy.com>
2022-02-11 15:31:38 +0000

    speeddddd

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#801', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 0.5 , 0.5 );
             view.addChild(circle);
             circles.push(circle);
         }
