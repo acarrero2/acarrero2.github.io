c44ac42a5fb20b85f6319bc7ff1f533f5786e8bd
acarrero2 <a.carrero@ardencahillacademy.com>
2022-03-16 14:39:09 +0000

    final product

--- projects/raccoonagram/javascript/brains.js
+++ projects/raccoonagram/javascript/brains.js
@@ -1,1 +1,17 @@
+var likeCount = [0, 0, 0, 0, 0, 0];
+function likePhoto(num) {
+    likeCount[num]++;
+    console.log(likeCount);
+    document.getElementById("likeCount" + num).innerHTML = likeCount[num];
+}
+var user = {
+    "name": "Angelina",
+    "name_last": "Carrero",
+    "followers": ["raccoon", "raccoon", "raccoon", "raccoon", "raccoon", "raccoon", "raccoon", "raccoon", "raccoon", "raccoon"],
+    "profile_pic": "https://i.pinimg.com/originals/42/b1/79/42b1795ed414b1dcaae150145e0ca537.jpg",
 
+}
+setTimeout(function () { document.getElementById("name").innerHTML = user.name; });
+setTimeout(function () { document.getElementById("name_last").innerHTML = user.name_last; });
+setTimeout(function () { document.getElementById("followers").innerHTML = "Followers: " + user.followers.length; });
+setTimeout(function () { $("#profile_pic").attr("src", user.profile_pic); });
